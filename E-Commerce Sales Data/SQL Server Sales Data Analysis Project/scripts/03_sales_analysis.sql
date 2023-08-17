a6a1fc79e18ab4195e64a007e18bcd0ffb412057
Dhopkins1225 <dhopkins1225@gmail.com>
2023-08-17 16:21:33 -0700

    Updated SQL query titles and saved outputs

--- E-Commerce Sales Data/SQL Server Sales Data Analysis Project/scripts/03_sales_analysis.sql
+++ E-Commerce Sales Data/SQL Server Sales Data Analysis Project/scripts/03_sales_analysis.sql
@@ -26,9 +26,25 @@ SELECT
      (SELECT COUNT(*) FROM Customers)) * 100 AS Retention_Rate_Percentage;
 
 
+--Combining Total Revenue, Avg Order Value and Retention Rate into one table
+WITH RepeatCustomers AS (
+    SELECT o.Customer_ID
+    FROM Orders o
+    GROUP BY o.Customer_ID
+    HAVING COUNT(DISTINCT o.Order_ID) > 1
+)
+
+SELECT 
+    (SELECT SUM(Amount) FROM Order_Details) AS Total_Revenue,
+    (SELECT AVG(Amount) FROM Order_Details) AS Average_Order_Value,
+    (CAST((SELECT COUNT(*) FROM RepeatCustomers) AS FLOAT) 
+     / 
+     (SELECT COUNT(*) FROM Customers)) * 100 AS Retention_Rate_Percentage;
 
 
---ProductSales Trends
+
+------------ProductSales Trends-----------------------------
+
 -- Sales by Category
 SELECT cat.Category_Name, SUM(od.Amount) AS Total_Sales
 FROM Order_Details od
@@ -44,8 +60,7 @@ FROM Customers c
 JOIN Orders o ON c.Customer_ID = o.Customer_ID;
 
 
-
---Combine Customer Data with Order Data:
+--Month over month Total Sales growth:
 WITH MonthlySales AS (
     SELECT 
         YEAR(o.Order_Date) AS Year,
@@ -67,14 +82,6 @@ SELECT
     END AS MoM_Growth_Percentage
 FROM MonthlySales
 ORDER BY Year, Month;
-
-
---Combine Category Data with Order Details
-SELECT cat.Category_Name, SUM(od.Amount) AS Total_Sales
-FROM Order_Details od
-JOIN Sub_Category subcat ON od.Sub_Category_ID = subcat.Sub_Category_ID
-JOIN Category cat ON subcat.Category_ID = cat.Category_ID
-GROUP BY cat.Category_Name;
 
 
 
